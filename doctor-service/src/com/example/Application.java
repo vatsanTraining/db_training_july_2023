4e62e2c0292f7901ea967342a0f95ef8f511f876
srivatsan <vatsank@gmail.com>
2023-07-28 15:56:15 +0530

    Update

--- doctor-service/src/com/example/Application.java
+++ doctor-service/src/com/example/Application.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 import javax.print.Doc;
 
@@ -13,20 +14,39 @@ public class Application {
 	public static void main(String[] args) {
 
 		Doctor ram = 
-				new Doctor(1020,"Ramkumar", "ent", 383823823);
+				new Doctor(1025,"Aravind", "ent", 173553823);
 		
 		CrudRepository<Doctor> repo = new DoctorService();
 		
-		System.out.println("Is Added :=>"+ repo.add(ram));
+		//System.out.println("Is Added :=>"+ repo.add(ram));
 		
 		
 		List<Doctor> list = repo.findAll();
 		
 		
-		for(Doctor eachDoctor:list) {
-			
-			System.out.println(eachDoctor);
-		}
+//		for(Doctor eachDoctor:list) {
+//			
+//			System.out.println(eachDoctor);
+//		}
+//		
+		
+		list.forEach(System.out::println);  //Method Reference
+		
+		// Predicate is a Functional Interface
+		
+		Predicate<Doctor> cond =
+				   (e) -> e.getDepartment().equalsIgnoreCase("ent"); //lambda
+				   
+				   
+
+				   System.out.println("Ent Doctors");
+					for(Doctor eachDoctor:list) {
+						
+						if(cond.test(eachDoctor)) {
+						   System.out.println(eachDoctor);
+						}
+					}
+				
 		
 	}
 
