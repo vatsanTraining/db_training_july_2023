81d2fc50c6043f031f19297e3286717af45d2e71
srivatsan <vatsank@gmail.com>
2023-07-28 13:04:51 +0530

    Day One Pre Lunch Commit

---
+++ doctor-service/src/com/example/utils/ConnectionFactory.java
@@ -0,0 +1,40 @@
+package com.example.utils;
+import java.io.*;
+import java.sql.DriverManager;
+import java.util.Properties;
+public class ConnectionFactory {
+
+	
+	
+	public static String[] readFromFile() {
+		
+		File file = new File("DbConnection.properties");
+		
+		String[] values = new String[3];
+		try {
+			InputStream instream = new FileInputStream(file);
+			
+			Properties props = new Properties();
+			
+			 props.load(instream);
+			
+			 values[0]= props.getProperty("datasource.url");
+			 values[1] = props.getProperty("datasource.username");
+			 values[2] = props.getProperty("datasource.password");
+			 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return values;
+	}
+	
+	public static void main(String[] args) {
+
+		for(String eachElement:readFromFile()) {
+			System.out.println(eachElement);
+		}
+	}
+}
