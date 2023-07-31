75c5632435d2fe71e9c557035274cb265b755d9e
srivatsan <vatsank@gmail.com>
2023-07-31 10:54:23 +0530

    Open API Swagger Dependency Added

--- doctor-service-rest-api/src/main/java/com/example/demo/DoctorServiceRestApiApplication.java
+++ doctor-service-rest-api/src/main/java/com/example/demo/DoctorServiceRestApiApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Doctor Management API"))
+@CrossOrigin(originPatterns = "*")
 public class DoctorServiceRestApiApplication {
 
 	public static void main(String[] args) {
